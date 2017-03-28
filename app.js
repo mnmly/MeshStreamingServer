a3a869daadeb1ccbb8943425de7e143379c6e376
mnmly <i.am@mnmly.com>
2017-03-28 01:12:09 -0700

    Added json support

--- app.js
+++ app.js
@@ -2,25 +2,36 @@ var receivedData
 
 // 1.モジュールオブジェクトの初期化
 var fs = require("fs");
+var port = process.env.PORT || 8888
 var server = require("http").createServer(function(req, res) {
      res.writeHead(200, {"Content-Type":"text/html"});
      var output = fs.readFileSync("./index.html", "utf-8");
      res.end(output);
-}).listen(8080);
+}).listen( port, function( err ) {
+  if ( err ) console.log( err.message )
+  console.log( "Running at port: " + port )
+} );
+
 var io = require("socket.io").listen(server);
 
 // ユーザ管理ハッシュ
 var userHash = {};
 
-// 2.イベントの定義
 io.sockets.on("connection", function (socket) {
 
-  // 接続開始カスタムイベント(接続元ユーザを保存し、他ユーザへ通知)
+  if ( receivedData ) {
+    if ( receivedData.mesh instanceof Buffer ) {
+      socket.emit("unity",{"mesh":receivedData.mesh.toString("base64")});
+    } else {
+      socket.emit("json",receivedData);
+    }
+  }
+
   socket.on("connected", function (name) {
     var msg = name + " is connected.";
     userHash[socket.id] = name;
     io.sockets.emit("publish", {value: msg});
-    console.log(msg);
+
   });
 
   // メッセージ送信カスタムイベント
@@ -42,10 +53,11 @@ io.sockets.on("connection", function (socket) {
   });
 
   socket.on("gh", function (data) {
-    console.log("data received: " + data.mesh.length);//data.mesh);
     receivedData = data;
-    io.sockets.emit("unity",{"mesh":data.mesh.toString("base64")});
-
+    if ( data.mesh instanceof Buffer ) {
+      io.sockets.emit("unity",{"mesh":data.mesh.toString("base64")});
+    } else {
+      io.sockets.emit("json",data);
+    }
   });
-
 });
